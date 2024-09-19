b915d6936f7a0ec9966ef4c407e4b37b34adbcf8
kalpna028 <87688686+kalpna028@users.noreply.github.com>
2024-09-19 14:06:08 +0530

    Add files via upload

---
+++ Storage/WeatherInfoStorage.swift
@@ -0,0 +1,51 @@
+//
+//  WeatherInfoStorage.swift
+//  WeatherForecast
+//
+//  Created by KalpnaMishra on 19/09/24.
+//
+
+import Foundation
+
+protocol WeatherInfoStorageManager {
+    func cacheWeatherInfo(weather: Weather)
+    func getAllStoredWeatherInfo()
+    func getWeatherInfo(forCity city: String) -> Weather?
+}
+
+class WeatherInfoStorage : WeatherInfoStorageManager {
+    
+    private var userDefaults = UserDefaults.standard
+    
+    init(userDefaults: UserDefaults = UserDefaults.standard) {
+        self.userDefaults = userDefaults
+    }
+    
+    func cacheWeatherInfo(weather: Weather) {
+        let encoder = JSONEncoder()
+        
+        var newWeather = weather
+        newWeather.timestamp = Date()
+        
+        if let encoded = try? encoder.encode(newWeather) {
+            userDefaults.set(encoded, forKey: newWeather.location?.name ?? "")
+        }
+    }
+    
+    func getAllStoredWeatherInfo() {
+        
+    }
+    
+    func getWeatherInfo(forCity city: String) -> Weather? {
+        let decoder = JSONDecoder()
+        
+        if let cachedWeather = userDefaults.data(forKey: city) {
+            if let cachedWeatherInfo = try? decoder.decode(Weather.self, from: cachedWeather) {
+                return cachedWeatherInfo
+            }
+        }
+        return nil
+    }
+    
+    
+}
