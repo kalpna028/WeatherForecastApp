125a098e5b078f9dd2cbbf02aabd50506aa9ccbb
kalpna028 <87688686+kalpna028@users.noreply.github.com>
2024-09-19 15:02:30 +0530

    Update WeatherViewModelTests.swift

--- WeatherForecastTests/WeatherViewModelTests.swift
+++ WeatherForecastTests/WeatherViewModelTests.swift
@@ -15,8 +15,70 @@ final class WeatherViewModelTests: XCTestCase {
         let mockAPIService = MockWeatherAPIService(session: mockSession)
         mockAPIService.isForSuccess = true
         let weatherViewModel = WeatherViewModel(weatherService: mockAPIService)
+    }
+    
+    func testForShouldMakeAPICallWhenCacheNotExpired() {
+        let mockSession = MockSession()
+        let mockAPIService = MockWeatherAPIService(session: mockSession)
+        mockAPIService.isForSuccess = true
+        let mockStoargeManager = MockWeatherInfoStorageManager()
+        let weatherViewModel = WeatherViewModel(weatherService: mockAPIService, weatherInfoStorageManager: mockStoargeManager)
+        let earlyDate = Calendar.current.date(
+          byAdding: .hour,
+          value: -3,
+          to: Date())
+        let isNeedToMakeAPICall = weatherViewModel.shouldMakeAPICall(savedDate: earlyDate!)
+        XCTAssertFalse(isNeedToMakeAPICall)
+    }
+    
+    func testForShouldMakeAPICallWhenCacheExpired() {
+        let mockSession = MockSession()
+        let mockAPIService = MockWeatherAPIService(session: mockSession)
+        mockAPIService.isForSuccess = true
+        let mockStoargeManager = MockWeatherInfoStorageManager()
+        let weatherViewModel = WeatherViewModel(weatherService: mockAPIService, weatherInfoStorageManager: mockStoargeManager)
+        let earlyDate = Calendar.current.date(
+          byAdding: .hour,
+          value: -7,
+          to: Date())
+        let isNeedToMakeAPICall = weatherViewModel.shouldMakeAPICall(savedDate: earlyDate!)
+        XCTAssertTrue(isNeedToMakeAPICall)
+    }
+    
+    func testForCheckWeatherCacheWithNoCache() {
+        let mockSession = MockSession()
+        let mockAPIService = MockWeatherAPIService(session: mockSession)
+        mockAPIService.isForSuccess = true
+        let mockStoargeManager = MockWeatherInfoStorageManager()
+        mockStoargeManager.isCacheAvailable = false
+        let weatherViewModel = WeatherViewModel(weatherService: mockAPIService, weatherInfoStorageManager: mockStoargeManager)
         
+        weatherViewModel.checkWeatherCache(forCity: "Delhi")
+        XCTAssertTrue(mockAPIService.isFetchWeatherCalled)
+    }
+    
+    func testForCheckWeatherCacheWithCache() {
+        let mockSession = MockSession()
+        let mockAPIService = MockWeatherAPIService(session: mockSession)
+        mockAPIService.isForSuccess = true
+        let mockStoargeManager = MockWeatherInfoStorageManager()
+        mockStoargeManager.isCacheAvailable = true
+        let weatherViewModel = WeatherViewModel(weatherService: mockAPIService, weatherInfoStorageManager: mockStoargeManager)
         
+        weatherViewModel.checkWeatherCache(forCity: "Delhi")
+        XCTAssertFalse(mockAPIService.isFetchWeatherCalled)
+    }
+    
+    func testForCheckWeatherCacheWithExpiredCache() {
+        let mockSession = MockSession()
+        let mockAPIService = MockWeatherAPIService(session: mockSession)
+        mockAPIService.isForSuccess = true
+        let mockStoargeManager = MockWeatherInfoStorageManager()
+        mockStoargeManager.isCacheAvailable = true
+        let weatherViewModel = WeatherViewModel(weatherService: mockAPIService, weatherInfoStorageManager: mockStoargeManager)
+        
+        weatherViewModel.checkWeatherCache(forCity: "Delhi")
+        XCTAssertFalse(mockAPIService.isFetchWeatherCalled)
     }
 
 }
