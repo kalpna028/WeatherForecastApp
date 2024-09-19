0b16c80cf4e840939034ffd58f69cb393f473a11
kalpna028 <87688686+kalpna028@users.noreply.github.com>
2024-09-19 14:10:37 +0530

    Update WeatherViewModel.swift

--- WeatherForecast/ViewModel/WeatherViewModel.swift
+++ WeatherForecast/ViewModel/WeatherViewModel.swift
@@ -12,17 +12,40 @@ class WeatherViewModel: ObservableObject {
     @Published var weather: Weather?
     @Published var error: CustomError?
     var weatherService = WeatherAPIService(session: Session())
+    private let weatherInfoStorageManager: WeatherInfoStorageManager
 
-    
-    init(weatherService:WeatherAPIService = WeatherAPIService(session: Session())) {
+    init(weatherService:WeatherAPIService = WeatherAPIService(session: Session()), weatherInfoStorageManager: WeatherInfoStorageManager = WeatherInfoStorage()) {
         self.weatherService = weatherService
+        self.weatherInfoStorageManager = weatherInfoStorageManager
     }
+    
+    func checkWeatherCache(forCity city: String) {
+        let cachedWeather = weatherInfoStorageManager.getWeatherInfo(forCity: city)
+        if let weather = cachedWeather, let savedDate = weather.timestamp {
+            let interval = Calendar.current.dateComponents([.hour], from: savedDate, to: Date())
+            
+            if interval.hour ?? 0 >= 6 {
+                makeAPICall(forCity: city)
+            } else {
+                //no api call needed
+                DispatchQueue.main.async {
+                    self.weather = weather
+                    self.error = nil
+                }
+            }
+        } else {
+            //API call
+            makeAPICall(forCity: city)
+        }
+    }
+    
+    
 
-
-    func fetchWeather(forCity city: String) {
+    func makeAPICall(forCity city: String) {
         weatherService.fetchWeather(forCity: city) { (weather, error)  in
             DispatchQueue.main.async {
                 if let weather = weather {
+                    self.weatherInfoStorageManager.cacheWeatherInfo(weather: weather)
                     self.weather = weather
                     self.error = nil
                 } else if let error = error {
