7801125bb6197c3cd2090a3c2df13d33abd75986
kalpna028 <87688686+kalpna028@users.noreply.github.com>
2024-09-19 14:09:38 +0530

    Update Weather.swift

--- WeatherForecast/Models/Weather.swift
+++ WeatherForecast/Models/Weather.swift
@@ -11,12 +11,14 @@ struct Weather : Codable {
     let location : Location?
     let current : Current?
     let forecast : Forecast?
+    var timestamp: Date?
 
     enum CodingKeys: String, CodingKey {
 
         case location = "location"
         case current = "current"
         case forecast = "forecast"
+        case timestamp = "timestamp"
     }
 
     init(from decoder: Decoder) throws {
@@ -24,12 +26,14 @@ struct Weather : Codable {
         location = try values.decodeIfPresent(Location.self, forKey: .location)
         current = try values.decodeIfPresent(Current.self, forKey: .current)
         forecast = try values.decodeIfPresent(Forecast.self, forKey: .forecast)
+        timestamp = try values.decodeIfPresent(Date.self, forKey: .timestamp)
     }
     
-    init(location: Location, current: Current, forecast: Forecast) {
+    init(location: Location, current: Current, forecast: Forecast, timestamp: Date) {
         self.location = location
         self.current = current
         self.forecast = forecast
+        self.timestamp = timestamp
     }
 
 }
